b242d9d1a71552fcb67f014aaf43bccb8f78df54
TheXmanz <52388234+BangDanz@users.noreply.github.com>
2020-03-06 21:08:19 +0700

    Add files via upload

---
+++ darkspam.sh
@@ -0,0 +1,214 @@
+#!/system/bin/bash
+
+clear
+
+echo "\033[032mSelamat Datang Di Tools DarkSpam"
+sleep 2
+
+clear
+
+                                          
+echo "\033[033m@@@@@@@    @@@@@@   @@@@@@@   @@@  @@@ "    
+echo "@@@@@@@@  @@@@@@@@  @@@@@@@@  @@@  @@@     "
+echo "?@@!  @@@  @@!  @@@  @@!  @@@  @@!  !@@     "
+echo "!@!  @!@  !@!  @!@  !@!  @!@  !@!  @!!     "
+echo "@!@  !@!  @!@!@!@!  @!@!!@!   @!@@!@!      "
+echo "!@!  !!!  !!!@!!!!  !!@!@!    !!@!!!       "
+echo "!!:  !!!  !!:  !!!  !!: :!!   !!: :!!      "
+echo ":!:  !:!  :!:  !:!  :!:  !:!  :!:  !:!     "
+echo " :::: ::  ::   :::  ::   :::   ::  :::     "
+echo ":: :  :    :   : :   :   : :   :   :::     "
+echo "                                         \033[032mAuthor\033[031m:\033[037mTheXManz"                                           
+echo "                                        \033[032mYoutub3\033[031m:\033[037mApmz channel"                                           
+echo "\033[036m @@@@@@   @@@@@@@    @@@@@@   @@@@@@@@@@ "  
+echo "@@@@@@@   @@@@@@@@  @@@@@@@@  @@@@@@@@@@@  "
+echo "!@@       @@!  @@@  @@!  @@@  @@! @@! @@!  "
+echo "!@!       !@!  @!@  !@!  @!@  !@! !@! !@!  "
+echo "!!@@!!    @!@@!@!   @!@!@!@!  @!! !!@ @!@  "
+echo " !!@!!!   !!@!!!    !!!@!!!!  !@!   ! !@!  "
+echo "     !:!  !!:       !!:  !!!  !!:     !!:  "
+echo "    !:!   :!:       :!:  !:!  :!:     :!:  "
+echo ":::: ::    ::       ::   :::  :::     ::   "
+echo ":: : :     :         :   : :   :      :    "
+echo "\033[032m================================================|"
+echo "================================================|"
+echo "\033[032m[\033[037m1\033[032m]\033[037m.\033[036m Spam Sms"
+sleep 1
+echo "\033[032m[\033[037m2\033[032m]\033[037m.\033[036m Spam Xl"
+sleep 1
+echo "\033[032m[\033[037m3\033[032m]\033[037m.\033[036m Spam Tokotalk"
+sleep 1
+echo "\033[032m[\033[037m4\033[032m]\033[037m.\033[036m Spam Tokopedia"
+sleep 1
+echo "\033[032m[\033[037m5\033[032m]\033[037m.\033[036m Spam Gmail"
+sleep 1
+echo "\033[032m[\033[037m6\033[032m]\033[037m.\033[036m Spam Telphone"
+sleep 1
+echo "\033[032m[\033[037m7\033[032m]\033[037m.\033[036m Spam Wa"
+sleep 1
+echo "\033[032m[\033[037m8\033[032m]\033[037m.\033[036m Spam KlikDokter"
+sleep 1
+echo "\033[032m[\033[037m9\033[032m]\033[037m.\033[036m Spam Rupa rupa"
+sleep 1
+echo "\033[032m[\033[037m10\033[032m]\033[037m.\033[036mSpam Pomona"
+sleep 1
+echo "\033[032m[\033[037m11\033[032m]\033[037m.\033[036mSpam Prosehat"
+sleep 1
+echo "\033[032m[\033[037m12\033[032m]\033[037m.\033[036mSpam Sepulsa"
+sleep 1
+echo "\033[032m[\033[037m13\033[032m]\033[037m.\033[036mSpam ShopBack"
+sleep 1
+echo "\033[032m[\033[037m14\033[032m]\033[037m.\033[036mSpam GlobalSms "
+sleep 1
+echo "\033[032m[\033[037m15\033[032m]\033[037m.\033[036mSpam Mypoin"
+sleep 1
+echo "\033[032m[\033[037m16\033[032m]\033[037m.\033[036mSpam vgsms"
+sleep 1
+echo "\033[032m[\033[037m17\033[032m]\033[037m.\033[036mSms Gratis"
+sleep 1
+echo "\033[032m[\033[037m18\033[032m]\033[037m.\033[036mSpam Olx"
+sleep 1
+echo "\033[032m[\033[037m19\033[032m]\033[037m.\033[036mSpam Redbus"
+sleep 1
+echo "\033[032m[\033[037m20\033[032m]\033[037m.\033[036mSpam Indomaret"
+sleep 1
+echo "\033[032m[\033[037m21\033[032m]\033[037m.\033[036mSpam Oyohotel"
+sleep 1
+echo "\033[032m[\033[037m22\033[032m]\033[037m.\033[036mSpam account kit"
+sleep 1
+echo "\033[032m[\033[037m99\033[032m]\033[037m.\033[036mInstall Bahan"
+sleep 1
+echo "\033[032m[\033[037m00\033[032m]\033[037m.\033[036mExit"
+sleep 1
+echo "\033[033m"
+read -p "@pilih nomor $:" pilih;
+if [ $pilih = "1" ]:
+then
+echo "Installing..."
+sleep 2
+cd tool
+bash spam-sms.sh
+if [$pilih = "2" ]:
+then
+cd tool
+bash spam-xl.sh
+
+if [ $pilih = "3" ]:
+then
+cd tool
+bash tokotalk.sh
+
+if [ $pilih = "4" ]:
+then
+cd tool
+bash wa.sh
+
+if [ $pilih = "5" ]:
+then
+cd tool
+bash gmail.sh
+
+if [ $pilih = "6" ]:
+then
+cd tool
+bash sms-call.sh
+
+if [ $pilih = "7" ]:
+then
+cd tool
+bash spam.sh
+
+if [ $pilih = "8" ]:
+then
+cd tool
+bash klikdokter.sh
+
+if [ $pilih = "9" ]:
+then
+cd tool
+bash spam.sh
+
+if [ $pilih = "10" ]:
+then
+cd tool
+bash pomona.sh
+
+if [ $pilih = "11" ]:
+then
+cd tool
+bash prosehat.sh
+
+if [ $pilih = "12" ]:
+then
+cd tool
+bash sepulsa.sh
+
+if [ $pilih = "13" ]:
+then
+cd tool
+bash shopback.sh
+
+if [ $pilih = "14" ]:
+then
+cd tool
+bash global.sh
+
+if [ $pilih = "15" ]:
+then
+cd tool
+bash spam.sh
+
+if [ $pilih = "16" ]:
+then
+cd tool
+bash spam.sh
+
+if [ $pilih = "17" ]:
+then
+cd tool
+bash sms.sh
+
+if [ $pilih = "18" ]:
+then
+cd tool
+bash olx.sh
+
+if [ $pilih = "19" ]:
+then
+cd tool
+bash redbus.sh
+
+if [ $pilih = "20" ]:
+then
+cd tool
+bash indomaret.sh
+
+if [ $pilih = "21" ]:
+then
+cd tool
+bash oyo.sh
+
+if [ $pilih = "99" ]:
+then
+sh install.sh
+
+if [ $pilih = "00" ]:
+then
+sleep 1
+echo "\033[032mTerimakasih Sudang Pakai Tools Ini"
+sleep 2
+echo "mohon luangkan waktu anda untuk sedikit membantu membangun channel saya"
+sleep 2
+echo "Subscribe Youtube Apmz Channel"
+sleep 2
+termux-open "https://www.youtube.com/channel/UCS4EpzW-9kWTIt3Dmzb4oQQ"
+sleep 3
+echo "bye Tod*_"
+sleep 2
+exit
+
+else
+echo "\033[032mInput Tidak ada\033[031m!!"
+sleep 2
+sh darkspam.sh
+fi
